54e690f16ef80ade35f98feb64b9e378a4e0471f
Ricardo Ruiz <ricardoruizfdez@gmail.com>
2024-06-10 16:19:30 +0200

    Case: Empty JSON

--- jrnl2tumblr/importer.py
+++ jrnl2tumblr/importer.py
@@ -2,5 +2,10 @@ import json
 
 def read_jrnl_file(jrnl_file_path):
     with open(jrnl_file_path, 'r') as file:
-        entries = json.load(file)
+        content = file.read()
+        if content.strip():
+            entries = json.load(file)
+        else:
+            print("The JSON file is empty or only contains whitespace")
+
     return entries
