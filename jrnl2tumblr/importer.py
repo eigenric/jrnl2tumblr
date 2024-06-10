6dabea10d22d7697c76be99ca807fa49ca8153f3
Ricardo Ruiz <ricardoruizfdez@gmail.com>
2024-06-10 16:26:31 +0200

    Test Passing

--- jrnl2tumblr/importer.py
+++ jrnl2tumblr/importer.py
@@ -2,10 +2,6 @@ import json
 
 def read_jrnl_file(jrnl_file_path):
     with open(jrnl_file_path, 'r') as file:
-        content = file.read()
-        if content.strip():
-            entries = json.load(file)
-        else:
-            print("The JSON file is empty or only contains whitespace")
+        entries = json.load(file)
 
     return entries
